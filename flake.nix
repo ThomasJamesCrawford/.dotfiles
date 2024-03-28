d460bdcf960e7b7d21c04f5a57d46abe455e7aba
Thomas Crawford <ThomasJamesCrawford96@gmail.com>
2024-03-28 22:20:39 +0800

    fix font

--- flake.nix
+++ flake.nix
@@ -8,20 +8,34 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs =
-    { nixpkgs
-    , home-manager
-    , ...
-    }:
+
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { system = system; };
+
+      font = pkgs.stdenv.mkDerivation {
+        name = "hack-font";
+        src = pkgs.fetchzip {
+          url = "https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip";
+          sha256 = "sha256-SxF4kYp9aL/9L9EUniquFadzWt/+PcvhUQOIOvCrFRM=";
+        };
+        installPhase = ''
+          mkdir -p $out/share/fonts/truetype
+          cp -r $src/* $out/share/fonts/truetype/
+        '';
+      };
+
     in
     {
       homeConfigurations = {
         thomas = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
+            {
+              home.packages = [ font ];
+            }
+
             ./home-manager/home.nix
           ];
         };
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+let
+  mac = pkgs.system == "x86_64-darwin";
+in
 {
   home.username = "thomas";
   home.homeDirectory = "/home/thomas";
@@ -29,6 +32,56 @@
   ]);
 
   home.file.".config/nvim/settings.lua".source = ./init.lua;
+
+  home.file.".config/alacritty/alacritty.yml".text = ''
+    # Colors (Gruvbox Material Dark Medium)
+    colors:
+      bright:
+        black: "0x928374"
+        blue: "0x7daea3"
+        cyan: "0x89b482"
+        green: "0xa9b665"
+        magenta: "0xd3869b"
+        red: "0xea6962"
+        white: "0xdfbf8e"
+        yellow: "0xe3a84e"
+
+      normal:
+        black: "0x665c54"
+        blue: "0x7daea3"
+        cyan: "0x89b482"
+        green: "0xa9b665"
+        magenta: "0xd3869b"
+        red: "0xea6962"
+        white: "0xdfbf8e"
+        yellow: "0xe78a4e"
+
+      primary:
+        background: "0x282828"
+        foreground: "0xdfbf8e"
+
+    env:
+      TERM: xterm-256color
+
+    font:
+      normal:
+        family: ${if mac then "MesloLGS NF" else "Hack"}
+        style: Regular
+
+      size: ${if mac then "16" else "13"}
+
+    shell:
+      program: ${pkgs.zsh}/bin/zsh
+      args:
+        - -l
+        - -c
+        - ${pkgs.tmux}/bin/tmux
+
+    ${if !mac then "background_opacity: 0.8" else ""}
+    window:
+      dynamic_padding: true
+      opacity: 0.8
+  '';
 
   programs.direnv.enable = true;
 
